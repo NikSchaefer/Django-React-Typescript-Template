42a9b354d8949120ba0ac9970523311d1be07f16
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-05 14:30:55 -0600

    Webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,32 @@
-import { resolve } from "path";
-import { DefinePlugin } from "webpack";
+const path = require("path");
+const webpack = require("webpack");
 
-export const entry = "./src/index.js";
-export const output = {
-    path: resolve(__dirname, "./static/frontend"),
-    filename: "main.js",
-    publicPath: resolve(__dirname, "./static/frontend")
-};
-export const module = {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./static/frontend"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
             },
-        },
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                // This has effect on the react lib size
+                NODE_ENV: JSON.stringify("production"),
+            },
+        }),
     ],
 };
-export const optimization = {
-    minimize: true,
-};
-export const plugins = [
-    new DefinePlugin({
-        "process.env": {
-            // This has effect on the react lib size
-            NODE_ENV: JSON.stringify("production"),
-        },
-    }),
-];
